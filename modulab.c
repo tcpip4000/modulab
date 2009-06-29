a3b81a0bde35ae9cc033f75740caa9302f4bfc8d
ertech <ertech@greenstar.site>
2009-06-29 13:54:52 -0500

    Added some files to the ignore rule

--- modulab.c
+++ modulab.c
@@ -6,15 +6,15 @@
 
 static int modulab(void)
 {
-	printk(KERN_INFO "modulab: modulab module being loaded \n");
-	printk(KERN_INFO "modulab: user space process is '%s' \n", current->comm);
-	printk(KERN_INFO "modulab: User space PID is '%i' \n", current->pid);
+	printk(KERN_INFO "modulab: modulab module being loaded. \n");
+	printk(KERN_INFO "modulab: user space process is '%s'. \n", current->comm);
+	printk(KERN_INFO "modulab: User space PID is '%i'. \n", current->pid);
 	return 0;
 }
 
 static void bye(void)
 {
-	printk(KERN_INFO "modulab: module being UNloaded");
+	printk(KERN_INFO "modulab: module being unloaded. \n");
 }
 
 module_init(modulab);
