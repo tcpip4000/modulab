57469b8d8ae44b703b30d7f50ae5eb45574dccaa
ertech <ertech@greenstar.site>
2009-06-29 20:18:54 -0500

    Added module parameters

--- modulab.c
+++ modulab.c
@@ -4,9 +4,17 @@
 #include <asm/current.h>
 #include <linux/sched.h>
 
+#include <linux/moduleparam.h>
+
+static char *whom = "world";
+static int howmany = 1;
+module_param(howmany, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
+
 static int modulab(void)
 {
 	printk(KERN_INFO "modulab: modulab module being loaded. \n");
+	printk(KERN_INFO "modulab: Hello %s. %i times. \n", whom, howmany);
 	printk(KERN_INFO "modulab: user space process is '%s'. \n", current->comm);
 	printk(KERN_INFO "modulab: User space PID is '%i'. \n", current->pid);
 	return 0;
